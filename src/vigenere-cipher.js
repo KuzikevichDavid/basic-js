d24037639312cf0c882718002d4fbc487c509951
Davyd <wliorjroo@gmail.com>
2023-04-16 21:10:07 +0300

    feat: implement `vigenere-cipher` task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,5 +1,3 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
 /**
  * Implement class VigenereCipheringMachine that allows us to create
  * direct and reverse ciphering machines according to task description
@@ -20,13 +18,65 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isDirect = true) {
+    this.isDirect = isDirect;
+    this.alhabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    this.indexes = Array(this.alhabet.length);
+    for (let i = 0; i < this.alhabet.length; i++) {
+      let char = this.alhabet.charAt(i);
+      this.indexes[char] = i;
+    }
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  baseCrypt(message, key, deCruptFlag = false) {
+    message = message.toUpperCase();
+    key = key.toUpperCase().split('');
+    let res = '';
+    for (let i = 0, j = 0; i < message.length; i++, j++) {
+      let curLetter = message.charAt(i);
+      if (this.alhabet.includes(curLetter)) {
+        let operationResult;
+        if (!deCruptFlag) {
+          operationResult = this.indexes[curLetter] + this.indexes[key[j]];
+        } else {
+          operationResult = this.indexes[curLetter] - this.indexes[key[j]];
+          if (operationResult < 0) {
+            operationResult = this.alhabet.length + operationResult;
+          }
+        }
+        let encryptedIndex = operationResult % this.alhabet.length;
+        res += this.alhabet.charAt(encryptedIndex);
+      } else {
+        res += curLetter;
+        j--;
+      }
+      if (j === key.length - 1) {
+        j = -1;
+      }
+    }
+    return res;
+  }
+
+  reverse(message) {
+    return message.split('').reverse().join('');
+  }
+
+  encrypt(message, key) {
+    if (!message || !key) throw new Error('Incorrect arguments!');
+    if (this.isDirect) {
+      return this.baseCrypt(message, key);
+    } else {
+      return this.baseCrypt(this.reverse(message), key);
+    }
+  }
+
+  decrypt(encryptedMessage, key) {
+    if (!encryptedMessage || !key) throw new Error('Incorrect arguments!');
+    if (this.isDirect) {
+      return this.baseCrypt(encryptedMessage, key, true);
+    } else {
+      return this.baseCrypt(this.reverse(encryptedMessage), key, true);
+    }
   }
 }
 
